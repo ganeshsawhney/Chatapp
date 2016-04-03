d495963f98be88430affc31bd4b93c9c4da75b8e
Ganesh Sawhney <ganeshsawhney@gmail.com>
2016-04-03 21:31:20 +0530

    Added custom message event

--- server.js
+++ server.js
@@ -6,8 +6,22 @@ var io = require('socket.io')(http)
 
 app.use(express.static(__dirname + '/public'))
 
-io.on('connection',function (){
+io.on('connection',function (socket){
 	console.log('User connected vis Socket.io');
+
+	socket.on('message', function (message) {
+		console.log('Message Recieved: ' + message.text);
+		//now the message has been recieved but it doesnot gets sent out to do this do below:
+
+		//send to everybody including person who sent it use io.emit
+		//send to everyone except person who sent it use below
+		socket.broadcast.emit('message', message);
+	});
+
+	//event, data to send
+	socket.emit('message',{ 
+		text: 'Welcome to the Chat Application'
+	});
 });
 
 http.listen(PORT, function(){
